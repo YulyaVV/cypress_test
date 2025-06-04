456edbf27d63639c075056a1df60877fc9f42822
YulyaVV <153502602+YulyaVV@users.noreply.github.com>
2025-06-04 13:07:02 +0700

    changed login

--- cypress/e2e/tests/login.cy.js
+++ cypress/e2e/tests/login.cy.js
@@ -2,14 +2,12 @@ import { MainPage } from '../../support/page_object/mainPage';
 
 describe('Проверка заголовка главной страницы', () => {
 
-
   beforeEach(() => {
-    cy.login('validUser');  
+    cy.login();  
     cy.visit('/admin/models');
   });
   
   it('Название страницы - "Список моделей"', () => {
     MainPage.getPageTitleText().should('eq', 'Список моделей');
   });
-
-})
+});
