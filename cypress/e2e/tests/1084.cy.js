2b2910351acc93e07c52578f824cf64675da538a
YulyaVV <153502602+YulyaVV@users.noreply.github.com>
2025-06-09 16:00:11 +0700

    tmp commit

--- cypress/e2e/tests/1084.cy.js
+++ cypress/e2e/tests/1084.cy.js
@@ -1,46 +1,74 @@
-import apiRequests from "../../support/api Requests";
+import apiRequests from "../../support/apiRequests";
 
 describe('Проверка атрибутов моделей', () => {
     
+    let modelWithoutAttributesId;
+    let modelWithAttributesId;
+
     before(() => {
-    cy.loginViaAPI();
+        cy.loginViaAPI();
 
-    cy.fixture('models').then((fixture) => {
+        cy.fixture('models').then((fixture) => {
 
-      // Создаем модель без атрибутов
-      apiRequests.createModel(fixture.baseModel);
-
-      // Создаем модель с атрибутами
-      apiRequests.createModel(fixture.baseModel).then((response) => {
-        cy.window().then((win) => {
-            cy.request({
-                method: 'POST',
-                url: `/web/models/${response.body.id}`,
-                headers: { 
-                    Authorization: `Bearer ${win.localStorage.getItem('token')}` 
-                },
-                body: {
-                    name: fixture.modelWithAttributes.name + Date.now(),
-                    description: fixture.modelWithAttributes.description,
-                    isActive: fixture.modelWithAttributes.isActive,
-                    attributes: fixture.modelWithAttributes.attributes,
-                    variants: fixture.modelWithAttributes.variants
-                }
+            // Создаем модель без атрибутов
+            apiRequests.createModel(fixture.baseModel).then((response)=> {
+                modelWithoutAttributesId = response.body.id;
             });
+
+            //тест создаем модель с аттр
+            apiRequests.createModel(fixture.baseModel).then((response) => {
+            cy.window().then((win) => {
+                cy.request({
+                        method: 'POST',
+                        url: `/web/models/${response.body.id}`,
+                        headers: { 
+                            Authorization: `Bearer ${win.localStorage.getItem('token')}` 
+                        },
+                        body: {
+                            name: fixture.modelWithAttributes.name + Date.now(),
+                            description: fixture.modelWithAttributes.description,
+                            isActive: fixture.modelWithAttributes.isActive,
+                            attributes: fixture.modelWithAttributes.attributes,
+                            variants: fixture.modelWithAttributes.variants
+                        }
+                }).then((response) => {
+                    modelWithAttributesId = response.body.id;
+                })
+            })
+        })
+
+            /*
+            // Создаем модель с атрибутами
+            apiRequests.createModel(fixture.baseModel).then((response) => {
+                cy.window().then((win) => {
+                    cy.request({
+                        method: 'POST',
+                        url: `/web/models/${response.body.id}`,
+                        headers: { 
+                            Authorization: `Bearer ${win.localStorage.getItem('token')}` 
+                        },
+                        body: {
+                            name: fixture.modelWithAttributes.name + Date.now(),
+                            description: fixture.modelWithAttributes.description,
+                            isActive: fixture.modelWithAttributes.isActive,
+                            attributes: fixture.modelWithAttributes.attributes,
+                            variants: fixture.modelWithAttributes.variants
+                        }
+                    });
+                });
+            });
+            */
         });
-        });
-    });
-})
+    })
 
    
-  beforeEach(() => {
-    cy.visitWithAuth('/admin/models?category=disc'); 
-    
-  });
+    beforeEach(() => {
+        cy.visitWithAuth('/admin/models?category=disc'); 
+    });
   
 
-  it ('не знаю какое придумать название теста', () => {
-    
-  });
+    it ('не знаю какое придумать название теста', () => {
+        
+    });
 })
 
