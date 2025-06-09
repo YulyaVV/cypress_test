27f2594deb514a65dedc9a586d333f4208964a3e
YulyaVV <153502602+YulyaVV@users.noreply.github.com>
2025-06-09 12:48:15 +0700

    added apiRequests class

---
+++ cypress/support/api Requests.js
@@ -0,0 +1,17 @@
+export default class apiRequests {
+    
+    static createModel (modelData) {
+    return cy.request({
+        method: 'POST',
+        url: '/web/models',
+        headers: { 
+            Authorization: `Bearer ${window.localStorage.getItem('token')}` 
+        },
+        body: {
+            name: modelData.name + Date.now(), // Добавляем timestamp для уникальности
+            category: modelData.category
+        }
+    });
+  };
+
+}
