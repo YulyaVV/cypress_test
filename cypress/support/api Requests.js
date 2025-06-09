189f963185c8a4e03a57dce219f6c26fec47b98f
YulyaVV <153502602+YulyaVV@users.noreply.github.com>
2025-06-09 14:13:40 +0700

    moved fixtures to Support chapter

--- cypress/support/api Requests.js
+++ cypress/support/api Requests.js
@@ -1,17 +1,16 @@
 export default class apiRequests {
-    
-    static createModel (modelData) {
-    return cy.request({
-        method: 'POST',
-        url: '/web/models',
-        headers: { 
-            Authorization: `Bearer ${window.localStorage.getItem('token')}` 
-        },
-        body: {
-            name: modelData.name + Date.now(), // Добавляем timestamp для уникальности
-            category: modelData.category
-        }
-    });
-  };
 
+    static createModel (modelData) {
+        return cy.request({
+            method: 'POST',
+            url: '/web/models',
+            headers: { 
+                Authorization: `Bearer ${window.localStorage.getItem('token')}` 
+            },
+            body: {
+                name: modelData.name + Date.now(), // Добавляем timestamp для уникальности
+                category: modelData.category
+            }
+        });
+    };
 }
